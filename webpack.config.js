788cc4f545578aba5efbcfe48cd27e5cc35f108d
Billy Derringer <billyderringer7@gmail.com>
2019-01-17 17:36:37 -0600

    update webpack

--- webpack.config.js
+++ webpack.config.js
@@ -34,8 +34,9 @@ module.exports = {
     ]
   },
   plugins: [
-    new HtmlWebpackPlugin({
-      template: './src/index.html'
-    })
+    new HtmlWebpackPlugin([
+      {template: './src/index.html'},
+      {from: './src/favicon.ico'}
+      ])
   ]
 }
