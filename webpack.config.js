c1282fef8023581188f3bd45e62e5a1b1b58b402
Billy Derringer <billyderringer7@gmail.com>
2019-01-17 17:46:56 -0600

    update webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
 
 module.exports = {
   entry: './src/index.js',
@@ -34,9 +35,11 @@ module.exports = {
     ]
   },
   plugins: [
-    new HtmlWebpackPlugin([
-      {template: './src/index.html'},
-      {favicon: './src/favicon.ico'}
-      ])
+    new HtmlWebpackPlugin({
+      template: './src/index.html'
+    }),
+    new CopyWebpackPlugin({
+      from: './src/favicon.ico'
+    })
   ]
 }
